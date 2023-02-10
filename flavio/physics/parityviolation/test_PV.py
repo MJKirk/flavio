1e83b418410096ba88bc5e03330f241a3737babf
Matthew Kirk <mjkirk@icc.ub.edu>
2023-02-10 17:49:17 +0100

    Whitespace fix

--- flavio/physics/parityviolation/test_PV.py
+++ flavio/physics/parityviolation/test_PV.py
@@ -6,4 +6,3 @@ class TestParityViolation(unittest.TestCase):
         # SM predictions and errors taken from 2107.13569
         self.assertAlmostEqual(flavio.sm_prediction("Q_W", Z=1, N=0), 0.0710, delta=0.0004)
         self.assertAlmostEqual(flavio.sm_prediction("Q_W", Z=55, N=78), -73.24, delta=0.05)
-    
